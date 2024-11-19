495b4434dde3c10538279101c788ca70e7bfaa62
0ero1ne <volikovd17@gmail.com>
2024-11-20 02:46:55 +0300

    [Lab 9] Done SHA-1

---
+++ Lab9/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+using Lab9.Hash;
+
+var message = "hello";
+var timer = Stopwatch.StartNew();
+var hashedMessage = SHA1.Hash(message);
+timer.Stop();
+Console.WriteLine($"H(\"{message}\") = {hashedMessage} in {timer.ElapsedMilliseconds} ms");
